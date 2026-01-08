e6b5d522a50cfaf30abe46733e4915c57bc04fec
Vincent Duchauffour <40724343+VDuchauffour@users.noreply.github.com>
2026-01-08 21:35:44 +0100

    feat: declare scaleway proxy with terraform (#1)
    * feat: declare scaleway proxy with terraform
    
    * add README.md
    
    * fix typo

---
+++ scaleway-proxy/terraform.tfvars
@@ -0,0 +1,7 @@
+instance_name    = "pris"
+instance_type    = "DEV1-S"
+image_id         = "ubuntu_noble"
+zone             = "fr-par-1"
+region           = "fr-par"
+root_volume_size = 20
+tags             = []
---
+++ scaleway-proxy/variables.tf
@@ -0,0 +1,41 @@
+variable "zone" {
+  description = "Scaleway zone"
+  type        = string
+  default     = "fr-par-1"
+}
+
+variable "region" {
+  description = "Scaleway region"
+  type        = string
+  default     = "fr-par"
+}
+
+variable "instance_name" {
+  description = "Instance name"
+  type        = string
+  default     = "dev-instance"
+}
+
+variable "instance_type" {
+  description = "Instance type"
+  type        = string
+  default     = "DEV1-S"
+}
+
+variable "image_id" {
+  description = "Image ID (e.g., Ubuntu 22.04)"
+  type        = string
+  default     = "ubuntu_jammy" # or specific UUID
+}
+
+variable "root_volume_size" {
+  description = "Root volume size in GB"
+  type        = number
+  default     = 20
+}
+
+variable "tags" {
+  description = "Instance tags"
+  type        = list(string)
+  default     = ["terraform", "dev"]
+}
